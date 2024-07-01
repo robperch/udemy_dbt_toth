0a23c8ca0741d9b10dfd98cdd6a26839b30f8210
Roberto919 <robertosysm@gmail.com>
2024-06-30 22:35:36 -0700

    creating snapshot for listings source

---
+++ dbtlearn/snapshots/snap_raw_listings.sql
@@ -0,0 +1,18 @@
+{% snapshot snap_raw_listings %}
+
+{{
+    config(
+        target_schema='dev',
+        unique_key='id',
+        strategy='timestamp',
+        updated_at='updated_at',
+        invalidate_hard_deletes=True
+    )
+}}
+
+select
+    *
+from
+    {{ source('airbnb', 'listings') }}
+
+{% endsnapshot %}
