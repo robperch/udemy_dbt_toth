d04dc647567d9216332f9c53a99b44454480b4a2
Roberto919 <robertosysm@gmail.com>
2024-07-08 19:27:27 -0700

    created macro and test to evaluate no null in columns

---
+++ dbtlearn/macros/no_nulls_in_columns.sql
@@ -0,0 +1,14 @@
+-- Code to test if there are any nulls in any columns
+{% macro no_nulls_in_columns(model) %}
+
+select
+    *
+from
+    {{ model }}
+where
+    {% for col in adapter.get_columns_in_relation(model) -%}
+        {{ col.column }} is null or
+    {% endfor %}
+    false
+
+{% endmacro %}
